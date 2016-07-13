5df230f208ea998b6d47326dd6757117cd2f3c38
lcorozco <lcorozco10@gmail.com>
2016-07-12 23:48:32 -0600

    create  header in the  http response

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
  * Created by lcorozco on 07-06-16.
  */
 var http = require("http"),
-    fs = require("fs");
+    fs = require("fs")
 var data = fs.readFileSync("./index.html");
 http.createServer(function(req, res){
     console.log("Hello, this is my first node js app");
-    res.write(data);
-    res.end("Bye bye....:)");
+    res.writeHead(201,{"Content-Type":"application/json"});
+    res.write(JSON.stringify({"Name":"Luis","LastName":"Orozco"}));
+    //res.write(data);
+    res.end();
 }).listen(3000);

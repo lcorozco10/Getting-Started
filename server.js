eddc201139d56c6800aa1c09da442022b28d034c
lcorozco <lcorozco10@gmail.com>
2016-08-02 00:03:23 -0600

    using module exports

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 /**
  * Created by lcorozco on 07-06-16.
  */
-var http = require("http"),
-    fs = require("fs")
-var data = fs.readFileSync("./index.html");
-http.createServer(function(req, res){
-    console.log("Hello, this is my first node js app");
-    res.writeHead(201,{"Content-Type":"application/json"});
-    res.write(JSON.stringify({"Name":"Luis","LastName":"Orozco"}));
-    //res.write(data);
-    res.end();
-}).listen(3000);
+var http = require("http"), fs = require("fs"), parse =  require("./lib/module.js");
+const server = http.createServer(function(req, res){
+    p = parse.parse;
+    res.writeHead(201,
+        {
+            /*"content-Type":"application/json"*/
+            "content-Type":"text/html"
+        }
+    );
+    fs.readFile("./index.html",function (err, html) {
+
+        var html_string = html.toString();
+        var variables = html_string.match(/[^\{\}]+(?=\})/g);
+        var paramsHash = p(req);
+        for(var i = variables.length - 1; i >= 0 ; i --){
+            //var value =  eval(variables[i].toString());
+            html_string = html_string.replace("{"+variables[i]+"}",paramsHash[variables[i]]);
+        }
+        res.write(html_string);
+        res.end();
+    });
+
+    //res.write(JSON.stringify({"Name":"Luis","LastName":"Orozco."}));
+});
+const hostname = '192.168.50.100';
+const port = 3000;
+server.listen(port,hostname,start);
+function start() {
+    console.log(`Server running at http://${hostname}:${port}/`);
+}
